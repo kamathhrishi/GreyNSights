c8c3d99d8a143d2d2a1b01c9c51f5c395ab9a04d
Hrishikesh Kamath <kamathhrishi@gmail.com>
2021-02-13 05:52:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 import setuptools
 
-"""with open("README.md", "r") as fh:
-    long_description = fh.read()"""
 
 setuptools.setup(
     name="GreyNsights",  # Replace with your own username
     version="0.0.1",
     author="Hrishikesh Kamath",
     author_email="kamathhrishi@gmail.com",
-    description="A small example package",
+    description="A Framework for Privacy-Preserving Data Analysis",
     long_description="",
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
