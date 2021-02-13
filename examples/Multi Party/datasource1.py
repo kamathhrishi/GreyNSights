6c762cb9b951fad5d19c5eb52446446da973a444
Hrishikesh Kamath <kamathhrishi@gmail.com>
2021-02-13 05:57:54 +0530

    Create datasource1.py

---
+++ examples/Multi Party/datasource1.py
@@ -0,0 +1,13 @@
+import pandas
+from GreyNsights.host import Dataset, DataOwner
+from GreyNsights.config import Config
+
+dataset = pandas.read_csv("week_data.csv")
+
+owner = DataOwner("Bob", port=65444, host="127.0.0.1")
+
+config = Config(owner)
+config.load("test_config.yaml")
+
+dataset = Dataset(owner, "Sample Data1", dataset, config, whitelist={"Alice": None})
+dataset.listen()
