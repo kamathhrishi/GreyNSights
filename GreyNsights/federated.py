d1e7328316fcc2e30ea5b505caf5096a96c21c55
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-08-06 11:01:26 +0530

    RRefractored example test and ran black

--- GreyNsights/federated.py
+++ GreyNsights/federated.py
@@ -1,9 +1,9 @@
 class Aggregagtion:
     def sum(results):
-        
+
         sum = 0.0
-        
+
         for item in results.keys():
-            sum+=results[item]
-            
+            sum += results[item]
+
         return sum
--- tests/test_carrots/test_carrots.py
+++ tests/test_carrots/test_carrots.py
@@ -1,1 +1,30 @@
-import socketimport codecsimport pickleimport pandasimport randomfrom GreyNsights.analyst import VirtualWorkerfrom GreyNsights.host import Dataset, DataOwnerfrom GreyNsights.config import Configdef test_example():   dataset = pandas.read_csv(      "animals_and_carrots.csv", sep=",", names=["animal", "carrots_eaten"]   )   config = Config()   config.load("test_config.yaml")   owner = VirtualWorker("Bob", config, data=dataset)   # owner = DataOwner("Bob", port=None, host=None,data=dataset)   df = owner.data.init_pointer()   assert (df.describe().get()==dataset.describe())["carrots_eaten"].all()   assert (df["carrots_eaten"].mean().get()==dataset.mean())["carrots_eaten"].all()   assert (df["carrots_eaten"].sum().get()==dataset.sum())["carrots_eaten"].all()   assert (((df["carrots_eaten"] > 70).sum().get())==(dataset["carrots_eaten"] > 70).sum())   assert df["carrots_eaten"].max().get()==dataset["carrots_eaten"].max()   
+import socket
+import codecs
+import pickle
+import pandas
+import random
+from GreyNsights.analyst import VirtualWorker
+from GreyNsights.host import Dataset, DataOwner
+from GreyNsights.config import Config
+
+
+def test_example():
+
+    dataset = pandas.read_csv(
+        "animals_and_carrots.csv", sep=",", names=["animal", "carrots_eaten"]
+    )
+
+    config = Config()
+    config.load("test_config.yaml")
+    owner = VirtualWorker("Bob", config, data=dataset)
+    # owner = DataOwner("Bob", port=None, host=None,data=dataset)
+
+    df = owner.data.init_pointer()
+
+    assert (df.describe().get() == dataset.describe())["carrots_eaten"].all()
+    assert (df["carrots_eaten"].mean().get() == dataset.mean())["carrots_eaten"].all()
+    assert (df["carrots_eaten"].sum().get() == dataset.sum())["carrots_eaten"].all()
+    assert ((df["carrots_eaten"] > 70).sum().get()) == (
+        dataset["carrots_eaten"] > 70
+    ).sum()
+    assert df["carrots_eaten"].max().get() == dataset["carrots_eaten"].max()
