33682a8735783c830d65913a49fd50833e442b42
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-08-09 18:38:53 +0530

    Fix tests

--- tests/test_carrots/test_carrots.py
+++ tests/test_carrots/test_carrots.py
@@ -8,7 +8,7 @@ from GreyNsights.config import Config
 
 
 @pytest.fixture
-def load_data():
+def init_pointer():
     data_path = os.path.join(os.path.dirname(__file__), "animals_and_carrots.csv")
     config_path = os.path.join(os.path.dirname(__file__), "test_config.yaml")
 
@@ -25,35 +25,34 @@ def load_data():
 
 
 def test_describe(init_pointer):
-
-    df, dataset = init_pointer()
+    df, dataset = init_pointer
     assert (df.describe().get() == dataset.describe())["carrots_eaten"].all()
 
 
 def test_mean(init_pointer):
-    df, dataset = init_pointer()
+    df, dataset = init_pointer
     assert (df["carrots_eaten"].mean().get() == dataset.mean())["carrots_eaten"].all()
 
 
 def test_sum(init_pointer):
-    df, dataset = init_pointer()
+    df, dataset = init_pointer
     assert (df["carrots_eaten"].sum().get() == dataset.sum())["carrots_eaten"].all()
 
 
 def test_sum_gt(init_pointer):
-    df, dataset = init_pointer()
+    df, dataset = init_pointer
     assert ((df["carrots_eaten"] > 70).sum().get()) == (
         dataset["carrots_eaten"] > 70
     ).sum()
 
 
 def test_sum_lt(init_pointer):
-    df, dataset = init_pointer()
+    df, dataset = init_pointer
     assert ((df["carrots_eaten"] < 70).sum().get()) == (
         dataset["carrots_eaten"] < 70
     ).sum()
 
 
 def test_max(init_pointer):
-    df, dataset = init_pointer()
+    df, dataset = init_pointer
     assert df["carrots_eaten"].max().get() == dataset["carrots_eaten"].max()
